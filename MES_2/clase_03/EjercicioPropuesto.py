915e6b2aeada8ad00462a5557ea8ea4f4ee49af1
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2024-03-21 22:47:28 -0500

    clase03-mes02

---
+++ MES_2/clase_03/EjercicioPropuesto.py
@@ -0,0 +1,34 @@
+"""
+Crear un Programa Para Gestionar una Biblioteca
+Representar Libros y bibliotecas
+
+1.-Clase Libro
+===========
+Atributos
+========
+-Titulo
+-Autor
+-Año de Publicacion
+-Genero
+
+Metodos
+=======
+__init__:inicializar los atributos del libro
+detalles:para mostrar los detalles del libro
+
+2.-Clase Biblioteca
+==================
+Atributos:
+==========
+-Nombre de la biblioteca
+-Lista de Libros(Lista Vacia)
+
+Metodos
+========
+__init__:inicializar los atributos de la biblioteca
+-Agregarlibro:Agregar un Libro a la lista de libros
+-Mostrarlibros:Mostrar los detalles de todos los libros en la biblioteca
+
+"""
+
+

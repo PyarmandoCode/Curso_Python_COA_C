4ee949e33e37f7812f84cf9ed92861a781a1cf3b
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2024-03-22 21:58:58 -0500

    clase04-mes02

--- MES_2/clase_03/EjercicioPropuesto.py
+++ MES_2/clase_03/EjercicioPropuesto.py
@@ -30,8 +30,6 @@ __init__:inicializar los atributos de la biblioteca
 -Mostrarlibros:Mostrar los detalles de todos los libros en la biblioteca
 
 """
-
-
 class Libro:
     def __init__(self,titulo,autor,año,genero):
         self.titulo=titulo
@@ -41,12 +39,41 @@ class Libro:
 
     def detalles(self):
         return f"Titulo:{self.titulo},Autor:{self.autor},Año{self.año},Genero {self.genero}"
+class Biblioteca:
+    def __init__(self,nombre):
+        self.nombre=nombre
+        self.libros=[]
+    def agregar_libro(self,libro):
+        self.libros.append(libro)    
+        print(f"Libro '{libro}' Agregando a la biblioteca")
+    def mostrar_libros(self):
+        print(f"Libros disponibles en la {self.nombre}")
+        for index,libro in enumerate(self.libros):
+            print(f"{index+1} - {libro.titulo}")    
+
+#Ejemplo de uso
+if __name__=="__main__":   
+    libro1=Libro("Cien años de soledad","Garcia Marques",1967,"Realismo")    
+    libro2=Libro("Don Quijote de la Mancha","Miguel Cervantes",1956,"Realismo")         
+    libro3=Libro("Aprende Python en 24 horas","Armando Ruiz",2024,"Programacion")
+    libro4=Libro("Aprende POO","Armando Ruiz",2023,"Programacion")
+    biblioteca=Biblioteca("Biblioteca Jesus Divino")#Creando una biblioteca
+    biblioteca.agregar_libro(libro1)
+    biblioteca.agregar_libro(libro2)
+    biblioteca.agregar_libro(libro3)
+    biblioteca.agregar_libro(libro4)
+    biblioteca.mostrar_libros()
+
+
+
+
+            
     
-libro1=Libro("Cien años de soledad","Garcia Marques",1967,"Realismo")    
-print(libro1.autor)
-libro1.año
-libro1.genero
-print(libro1.__dict__)
+
+#print(libro1.autor)
+#libro1.año
+#libro1.genero
+#print(libro1.__dict__)
 
 
 
